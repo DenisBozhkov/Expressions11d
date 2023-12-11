837c3d57418e5670e20a56c14d86fb5784b6f415
Денис Божков <stu2201261006@uni-plovdiv.bg>
2023-12-11 15:44:36 +0200

    Initial commit

---
+++ 11d/ISolvable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11d
+{
+    internal interface ISolvable
+    {
+        double[] Solve();
+        double[] Evaluate(double a);
+    }
+}
---
+++ 11d/Polynomial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11d
+{
+    internal abstract class Polynomial : IExpression, ISolvable
+    {
+        protected double[] coefficients;
+        public Polynomial(double[] coefficients)
+        {
+            this.coefficients = coefficients;
+        }
+        public int Power
+        { get { return coefficients.Length - 1; } }
+        public double Calculate(double x)
+        {
+            double res = coefficients[Power];
+            for (int i = Power - 1; i >= 0; i--)
+            {
+                res = res * x + coefficients[i];
+            }
+            return res;
+        }
+        public abstract double[] Solve();
+        public abstract double[] Evaluate(double a);
+    }
+}
